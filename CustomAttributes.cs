fff40ff70ce79d36476772ab4412b1e70f3c4043
jramsden271 <jramsden271@gmail.com>
2015-11-25 16:13:42 +0000

    Completely changed file
    Much better written this time, the input parameters line up properly now. Output parameters still to do.

--- CustomAttributes.cs
+++ CustomAttributes.cs
@@ -6,35 +6,95 @@ using Grasshopper.Kernel.Types;
 using System;
 using System.Drawing;
 
-namespace SCA.test_interface
+namespace SBAFile._1b_SBA_tidy
 {
-    public class CustomAttributes : GH_ComponentAttributes
+    public class CustomAttributes2 : GH_ComponentAttributes
     {
-        int paramheight;
-        int paramwidthinput;
-        int paramwidthoutput;
+        int titleheight = -1;
+        int titlemargin = 5;
+        int centrewidth = 100;
+        int inputwidth = -1;
+        int outputwidth = -1;
+        int paramheight = -1;
+        int bottommargin = 5;
+        int parammargin = 2;
 
-        public CustomAttributes(IGH_Component owner)
+        int componentheight = -1;
+        int componentwidth = -1;
+        PointF componentpivot = new PointF();
+
+        int maxnoofinputs = -1;
+
+        Color bgcolour = Color.FromArgb(255, 203, 198, 190);
+        Color bgcolour_selected = Color.FromArgb(255, 139, 224, 59);
+        Color bgcolour_working = Color.FromArgb(255, 50, 50, 150);
+        Color paramfontcolour = Color.FromArgb(255, 124, 124, 124);
+        Color titlebgcolour = Color.FromArgb(255, 94, 92, 89);
+        Color parambgcolour = Color.FromArgb(255, 228, 225, 221);
+
+        Font paramfont = new Font("ubuntu", 9f);
+        Font titlefont = new Font("ubuntu", 13f);
+        Font messagefont = new Font("ubuntu", 8.5f);
+
+        RectangleF componentBox;
+        RectangleF titleBox;
+
+        public CustomAttributes2(IGH_Component owner)
             : base(owner)
         {
         }
-        
+
         protected override void Layout()
         {
-            base.Layout();
+            //base.Layout();
             
-            this.Pivot = GH_Convert.ToPoint(this.Pivot);
-            this.Bounds = new RectangleF(this.Pivot, new SizeF((float)(paramwidthoutput + paramwidthinput + 50), (float)(Math.Max(Owner.Params.Input.Count * paramheight, Owner.Params.Output.Count * paramheight)) + 40f));
-            RectangleF componentBox = new RectangleF(this.Bounds.Location, this.Bounds.Size);
-            componentBox.X += 8f;
-            componentBox.Y += 30f;
-            componentBox.Width -= 18f;
-            componentBox.Height -= 40f;
-            GH_ComponentAttributes.LayoutInputParams(base.Owner, componentBox);
-            GH_ComponentAttributes.LayoutOutputParams(base.Owner, componentBox);
-            
+            //calc param width
+
+            foreach (var param in Owner.Params.Input)
+            {
+                inputwidth = Math.Max(inputwidth, GH_FontServer.MeasureString(param.Name, paramfont).Width);
+                paramheight = Math.Max(paramheight, GH_FontServer.MeasureString(param.Name, paramfont).Height);
             }
-         
+            foreach (var param in Owner.Params.Output)
+            {
+                outputwidth = Math.Max(outputwidth, GH_FontServer.MeasureString(param.Name, paramfont).Width);
+                paramheight = Math.Max(paramheight, GH_FontServer.MeasureString(param.Name, paramfont).Height);
+            }
+
+
+
+            Pivot = GH_Convert.ToPoint(Pivot);
+            //this.Bounds = new RectangleF(this.Pivot, new SizeF((float)(componentwidth + inputwidth + outputwidth), (float)(titleheight + titlemargin + bottommargin + (Math.Max(Owner.Params.Input.Count * paramheight, Owner.Params.Output.Count * paramheight)))));
+            centrewidth = Math.Max(centrewidth, GH_FontServer.MeasureString(Owner.Message, messagefont).Width + 5);
+            Size sizeedit = new Size(centrewidth, (2 * parammargin + paramheight) * Math.Max(Owner.Params.Input.Count, Owner.Params.Output.Count));
+            componentBox = new RectangleF(Pivot, sizeedit);            
+            
+            LayoutInputParamsJR(base.Owner, componentBox);
+            GH_ComponentAttributes.LayoutOutputParams(base.Owner, componentBox);
+
+            componentpivot = new PointF(componentBox.Location.X, componentBox.Location.Y);
+            componentwidth = centrewidth;
+            int fudge = 3; //accounts for the fact that GH adds a small margin when creating parameter attributes
+            if (Owner.Params.Input.Count > 0)
+            {
+                componentwidth += (int)(Owner.Params.Input[0].Attributes.Bounds.Width + fudge);
+                componentpivot.X -= (Owner.Params.Input[0].Attributes.Bounds.Width + fudge);
+            }
+            if (Owner.Params.Output.Count > 0)
+            {
+                componentwidth += (int)(Owner.Params.Output[0].Attributes.Bounds.Width + fudge);
+            }
+            componentpivot.Y -= (titleheight + titlemargin);
+            titleheight = GH_FontServer.MeasureString(Owner.Name, titlefont).Height;
+            componentheight = (int)componentBox.Height + titleheight + titlemargin + bottommargin;
+
+
+            titleBox = new RectangleF(componentpivot, new SizeF(componentwidth, titleheight));
+            Bounds = new RectangleF(componentpivot, new SizeF(componentwidth, componentheight));
+            
+
+        }
+
 
         public override void ExpireLayout()
         {
@@ -44,89 +104,115 @@ namespace SCA.test_interface
 
 
 
+
         protected override void Render(GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
         {
+            
             if (channel == GH_CanvasChannel.Wires)
             {
                 base.Render(canvas, graphics, channel);
-                this.Layout();
+                Layout();
             }
             if (channel == GH_CanvasChannel.Objects)
             {
+                //debug
+                /*
+                //base.Render(canvas, graphics, channel);
+                graphics.DrawRectangle(new Pen(Color.Yellow, 3f), GH_Convert.ToRectangle(Bounds));
 
+                
 
-                paramheight = 0;
-                //calc width and height
-                Font paramfont = new Font("ubuntu", 12f);
-                int inputwidth = 0;
-                foreach (var inp in this.Owner.Params.Input)
+                graphics.DrawRectangle(new Pen(Color.Blue, 2f), GH_Convert.ToRectangle(componentBox));
+
+                foreach (var param in Owner.Params.Input)
                 {
-                    inputwidth = Math.Max((int)(graphics.MeasureString(inp.Name, paramfont).Width), inputwidth);
-                    paramheight = Math.Max(paramheight, (int)(graphics.MeasureString(inp.Name, paramfont).Height));
+                    graphics.DrawRectangle(new Pen(Color.Green, 2f), GH_Convert.ToRectangle(param.Attributes.Bounds));
                 }
-                int outputwidth = 0;
-                foreach (var inp in this.Owner.Params.Output)
+                foreach (var param in Owner.Params.Output)
                 {
-                    outputwidth = Math.Max((int)(graphics.MeasureString(inp.Name, paramfont).Width), outputwidth);
-                    paramheight = Math.Max(paramheight, (int)(graphics.MeasureString(inp.Name, paramfont).Height));
+                    graphics.DrawRectangle(new Pen(Color.Green, 2f), GH_Convert.ToRectangle(param.Attributes.Bounds));
                 }
-                paramheight += 2;
-                paramwidthinput = inputwidth;
-                paramwidthoutput = outputwidth;
 
+                graphics.DrawRectangle(new Pen(Color.Red, 2f), new Rectangle((int)(Bounds.X), (int)(Bounds.Y), (int)(Bounds.Width), (int)(titleheight)));
+                */
 
-
-                this.Layout();
-                graphics.FillRectangle(new SolidBrush(Color.FromArgb(255, 203, 198, 190)), Rectangle.Round(this.Bounds));
-
-                Pen pen;
-                SolidBrush parambrush;
-                SolidBrush brush;
-
-                pen = new Pen(Color.FromArgb(160, 160, 160, 160), 1f);
-                brush = new SolidBrush(Color.FromArgb(255, 0, 0, 160));
-                parambrush = new SolidBrush(System.Drawing.Color.Red);
+                //background
+                if (Owner.Attributes.Selected) graphics.FillRectangle(new SolidBrush(bgcolour_selected), Bounds);
+                else graphics.FillRectangle(new SolidBrush(bgcolour), Bounds);
 
                 //title
-
-                var titlerectangle = new Rectangle((int)(this.Pivot.X), (int)(Pivot.Y), (int)(Bounds.Width), 26);
-                graphics.FillRectangle(new SolidBrush(Color.FromArgb(255, 94, 92, 89)), titlerectangle);
-                
-                var titlesize = graphics.MeasureString(this.Owner.Name, new Font("ubuntu", 12f));
-                var titleloc = new PointF((float)(titlerectangle.X + titlerectangle.Width * 0.5 - titlesize.Width * 0.5), (float)(titlerectangle.Y + 3));
-                graphics.DrawString(this.Owner.Name, new Font("ubuntu", 12f), Brushes.White, titleloc);
-
+                graphics.FillRectangle(new SolidBrush(titlebgcolour), titleBox);
+                float titlewidth = GH_FontServer.StringWidth(Owner.Name, titlefont);
+                graphics.DrawString(Owner.Name, titlefont, Brushes.White, new PointF((float)(titleBox.Left + 0.5 * titleBox.Width - 0.5 * titlewidth), titleBox.Top));
 
                 //params
-                var parambgcol = new SolidBrush(Color.FromArgb(255, 228, 225, 221));
-                foreach (var inp in this.Owner.Params.Input)
+                foreach (var param in Owner.Params.Input)
                 {
-                    graphics.FillEllipse(new SolidBrush(Color.Black), (float)(inp.Attributes.Pivot.X - 4), (float)(inp.Attributes.Pivot.Y - 4), 8f, 8f);
-
-                    var textloc = new PointF((float)(inp.Attributes.Pivot.X + 1), (float)(inp.Attributes.Pivot.Y - 10)); 
-
-                    var size = graphics.MeasureString(inp.Name, paramfont);
-                    size.Width = inputwidth;
-                    RectangleF rect = new RectangleF(textloc, size);
-                    graphics.FillRectangle(parambgcol, rect);
-                    graphics.DrawString(inp.Name, paramfont, new SolidBrush(Color.FromArgb(255, 124, 124, 124)), textloc);
+                    graphics.FillRectangle(new SolidBrush(parambgcolour), param.Attributes.Bounds);
+                    graphics.DrawString(param.Name, paramfont, new SolidBrush(paramfontcolour), new PointF(param.Attributes.Bounds.X, param.Attributes.Bounds.Y + parammargin));
                 }
-                foreach (var inp in this.Owner.Params.Output)
+                //params
+                foreach (var param in Owner.Params.Output)
                 {
-                    graphics.FillEllipse(new SolidBrush(Color.Black), (float)(inp.Attributes.Pivot.X - 4), (float)(inp.Attributes.Pivot.Y - 4), 8f, 8f);
-
-                    var textloc = new PointF((float)(inp.Attributes.Pivot.X -1), (float)(inp.Attributes.Pivot.Y - 10));
-
-                    var size = graphics.MeasureString(inp.Name, paramfont);
-                    size.Width = outputwidth;
-                    var textlocmod = new PointF(textloc.X - size.Width, textloc.Y);
-                    RectangleF rect = new RectangleF(textlocmod, size);
-                    graphics.FillRectangle(parambgcol, rect);
-                    graphics.DrawString(inp.Name, paramfont, new SolidBrush(Color.FromArgb(255, 124, 124, 124)), textlocmod);
+                    graphics.FillRectangle(new SolidBrush(parambgcolour), param.Attributes.Bounds);
+                    int textwidth = GH_FontServer.StringWidth(param.Name, paramfont);
+                    graphics.DrawString(param.Name, paramfont, new SolidBrush(paramfontcolour), new PointF(param.Attributes.Bounds.X, param.Attributes.Bounds.Y + parammargin));
                 }
 
-                //border
-                graphics.DrawRectangle(new Pen(Color.FromArgb(255, 94, 92, 89), 1f), new Rectangle((int)Bounds.X, (int)Bounds.Y, (int)Bounds.Width, (int)Bounds.Height));
+                //message
+                graphics.DrawString(Owner.Message, messagefont, Brushes.Gray, componentBox.Location);
+
+            }
+        }
+
+
+
+
+
+
+
+        // Grasshopper.Kernel.Attributes.GH_ComponentAttributes
+        public void LayoutInputParamsJR(IGH_Component owner, RectangleF componentBox)
+        {
+            int count = owner.Params.Input.Count;
+            if (count == 0)
+            {
+                return;
+            }
+            int width = 0;
+            try
+            {
+                System.Collections.Generic.List<IGH_Param>.Enumerator enumerator = owner.Params.Input.GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    IGH_Param param = enumerator.Current;
+                    width = Math.Max(width, GH_FontServer.StringWidth(param.Name, paramfont));
+                }
+            }
+            finally
+            {
+            }
+            width = Math.Max(width + 6, 12);
+            float pixelsPerParam = componentBox.Height / count;
+            int arg_82_0 = 0;
+            int num = count - 1;
+            for (int i = arg_82_0; i <= num; i++)
+            {
+                IGH_Param param2 = owner.Params.Input[i];
+                if (param2.Attributes == null)
+                {
+                    param2.Attributes = new GH_LinkedParamAttributes(param2, owner.Attributes);
+                }
+                float X = componentBox.X - width;
+                float Y = componentBox.Y + i * pixelsPerParam;
+                float W = width - 3;
+                float H = pixelsPerParam;
+                IGH_Attributes arg_103_0 = param2.Attributes;
+                PointF pivot = new PointF(X + 0.5f * width, Y + 0.5f * pixelsPerParam);
+                arg_103_0.Pivot = pivot;
+                IGH_Attributes arg_12A_0 = param2.Attributes;
+                RectangleF @in = new RectangleF(X, Y, W, H);
+                arg_12A_0.Bounds = GH_Convert.ToRectangle(@in);
             }
         }
     }
